faad12e4bef2bbbcdb89122456b07ecf9198b15e
Lajst <wojciechowski.kamil1@outlook.com>
2022-11-11 21:47:17 +0100

    Test2

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git -C ~/Mirror/ reset --hard 
-git -C ~/Mirror/ pull 
+git -C ~/Mirror reset --hard 
+git -C ~/Mirror pull 
 python ~/Mirror/buttons.py &
 python ~/Mirror/motion.py &
 python ~/Mirror/start_listener.py &
