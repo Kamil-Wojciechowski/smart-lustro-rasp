4c8de382e94a4d9d5df0a2645b6a6ed4f67b637c
Lajst <wojciechowski.kamil1@outlook.com>
2022-11-11 22:10:12 +0100

    Test6

--- gitChanges.py
+++ gitChanges.py
@@ -1,6 +1,5 @@
 import os
 
-os.system('pwd')
-os.system('git -C "/home/admin/Mirror/" reset --hard')
-os.system('git -C "/home/admin/Mirror/" pull')
+os.system('git -C home/admin/Mirror/ reset --hard')
+os.system('git -C home/admin/Mirror/ pull')
 
