ff9b29497deaca1b8c531e3a97b12fac9f2d8a19
Sxymi <81798280+Sxymi@users.noreply.github.com>
2022-11-18 20:25:17 +0100

    Update listener.py

--- Handlers/listener.py
+++ Handlers/listener.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from Handlers import config, database
 from Sensors import p_dht
+import requests
 
 application = Flask(__name__)
 CORS(application)
@@ -20,6 +21,11 @@ def is_awake():
     }
     return jsonify(information)
 
+@application.route('/get_outside_weather', method=['GET'])
+def get_outside_weather():
+    url = "https://api.openweathermap.org/data/2.5/forecast?lat=51.9383777&lon=15.5050408&appid=1610df520907691bbe49c15333e337b2"
+    response = requests.get(url)
+    return jsonify(response.text)
 
 def start_application():
     application.run(host=config.get_property('listener_host'), port=config.get_property('listener_port'))
